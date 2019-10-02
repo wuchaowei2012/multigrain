827d4b66a75b47b077e4e878be99001229254368
wuchaowei2012 <wuchaowei2015@gmail.com>
2019-10-02 19:28:04 +0800

    newest

--- start_extract_descriptor.sh
+++ start_extract_descriptor.sh
@@ -1,5 +1,10 @@
-export CUDA_VISIBLE_DEVICES=0
-export DATA_SET='meizi'
+export CUDA_VISIBLE_DEVICES=$1
+
+export starts=$2
+echo 2: starts, $starts
+
+export embeddingFilePath=$1
+echo 1: embeddingFilePath , $embeddingFilePath
 
 
-python3 scripts/extract_scriptor.py --expdir experiments/joint_3B_0.5/eval_p4_500 --input-size 200 --dataset meizi --pooling-exponent 4 --resume-from joint_3B_0.5.pth
+python3 scripts/extract_scriptor.py --embeddingFilePath=$embeddingFilePath, --starts $starts --expdir experiments/joint_3B_0.5/eval_p4_500 --input-size 200  --pooling-exponent 4 --resume-from joint_3B_0.5.pth
