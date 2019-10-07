dbd80368e91e5b5269d9ecf38a877c57b98d5a46
root <wuchaowei2015@gmail.com>
2019-10-07 14:32:00 +0800

    newest

--- host135_1.sh
+++
@@ -1,5 +0,0 @@
-./start_extract_descriptor.sh 0 /nfs/nfs135/videos/short_video_pic 5  /devdata/videos/short_s5_h135.txt
-./start_extract_descriptor.sh 0 /nfs/nfs135/videos/short_video_pic 6  /devdata/videos/short_s6_h135.txt
-./start_extract_descriptor.sh 0 /nfs/nfs135/videos/short_video_pic 7  /devdata/videos/short_s7_h135.txt
-./start_extract_descriptor.sh 0 /nfs/nfs135/videos/short_video_pic 8  /devdata/videos/short_s8_h135.txt
-./start_extract_descriptor.sh 0 /nfs/nfs135/videos/short_video_pic 9  /devdata/videos/short_s9_h135.txt
